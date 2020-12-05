4d05051d0532ff8a52431263883a3cbf51ced1e8
KirbyPaint <porter.ashley13@gmail.com>
2020-12-05 13:53:33 -0800

    minor changes and final commit for now

--- js/scripts.js
+++ js/scripts.js
@@ -31,17 +31,16 @@ $(document).ready(function() {
 
   $("form#questions").submit(function(event) {
     event.preventDefault();
-      const choiceOne = parseInt($("input:radio[name=q1]:checked").val()); // Need to change this to get the value of the selected radio buttons
-      //Hope the parseInt works, if not remove it and make it a function or something
+      const choiceOne = parseInt($("input:radio[name=q1]:checked").val());
       const choiceTwo = parseInt($("input:radio[name=q2]:checked").val());
       const choiceThree = parseInt($("input:radio[name=q3]:checked").val());
       const choiceFour = parseInt($("#q4").val());
       const choiceFive = parseInt($("input:radio[name=q5]:checked").val());
-      console.log(choiceOne); // None of this is writing to the console
+      console.log(choiceOne);
       console.log(choiceTwo);
       console.log(choiceThree);
       console.log(choiceFour);
       console.log(choiceFive);
-      $("#output").text(choiceOne);
+      $("#output").text(choiceOne + choiceTwo + choiceThree + choiceFour + choiceFive);
    });
 });
